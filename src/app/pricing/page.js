a4014ccafabbc6edd7f0df29cefee79d64f8e1d1
ekkymulia <ekky.mulialasardi@gmail.com>
2024-10-28 14:53:27 +0700

    change business model

--- src/app/pricing/page.js
+++ src/app/pricing/page.js
@@ -49,20 +49,98 @@ export default function Pricing() {
                           className="mb-[30px] h-[60px] w-auto"
                         />
                         <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-2xl">
-                          Paket per Semester
+                          Paket Institusi Besar
                         </div>
                         <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-xl flex gap-2 items-center">
-                          <span className="line-through">IDR 10.850.000,-</span>  <Chip color="primary">IDR 5.450.000,- (50%)</Chip>
+                          <span className="line-through">IDR 21.142.740,-</span>  <Chip color="primary">IDR 10.571.370,- (50%)</Chip>
                         </div>
                         <p className="my-0"><strong>Yang akan anda dapatkan:</strong></p>
                         <ul className="mb-5">
-                          <li><strong>- 10x Upload Pelajaran Berbeda</strong></li>
-                          <li><strong>- 10x Ruang Ujian</strong></li>
-                          <li><strong>- 150x Sesi Siswa Mengerjakan</strong></li>
-                          <li><strong>- 4500x Generasi dan Analisa soal menggunakan AI</strong></li>
-                          <li className="mt-4"><strong>Kuota Sistem:</strong></li>
+                          <li><strong>- 45x Sesi Ujian</strong></li>
+                          <li><strong>- 35x Siswa per Sesi Ujian</strong></li>
+                          <li><strong>- 15x Soal per Sesi Siswa</strong></li>
+                          <li><strong>- 23.625 Soal di Evaluasi Otomatis </strong></li>
+                          {/* <li className="mt-4"><strong>Kuota Sistem:</strong></li>
                           <li><strong>- 1x Kuota Pembuatan Institusi</strong></li>
-                          <li><strong>- 12+ Akun Multi Role (2x Akun Admin, 10x Akun Pengajar, Unlimited Akun Siswa)</strong></li>
+                          <li><strong>- 12+ Akun Multi Role (2x Akun Admin, 10x Akun Pengajar, Unlimited Akun Siswa)</strong></li> */}
+                        </ul>
+                        <div className="mt-auto w-full flex justify-end items-end">
+                          <a
+                            href="https://bit.ly/contact-evaloexam"
+                            className="w-full btn is-outline-black btn-animation is-large inline-block rounded justify-end items-end"
+                          >
+                            <span>Kontak Kami</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Feature Item */}
+                  {/* Feature Item */}
+                  <div className="jos" data-jos_delay={0}>
+                    <div className="hover-solid-shadow h-full">
+                      <div className="justify-start items-start h-full flex flex-col rounded-[10px] border-2 border-ColorBlack bg-white p-[30px]">
+                        <img
+                          src="https://masco-tailwind.netlify.app/assets/img/icons/icon-duotone-user-list.svg"
+                          alt="icon-duotone-user-list"
+                          width={60}
+                          height={60}
+                          className="mb-[30px] h-[60px] w-auto"
+                        />
+                        <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-2xl">
+                          Paket Institusi Sedang
+                        </div>
+                        <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-xl flex gap-2 items-center">
+                          <span className="line-through">IDR 6.816.015,-</span>  <Chip color="primary">IDR 3.408.000,- (50%)</Chip>
+                        </div>
+                        <p className="my-0"><strong>Yang akan anda dapatkan:</strong></p>
+                        <ul className="mb-5">
+                          <li><strong>- 30x Sesi Ujian</strong></li>
+                          <li><strong>- 25x Siswa per Sesi Ujian</strong></li>
+                          <li><strong>- 10x Soal per Sesi Siswa</strong></li>
+                          <li><strong>- 7.500 Soal di Evaluasi Otomatis </strong></li>
+                          {/* <li className="mt-4"><strong>Kuota Sistem:</strong></li>
+                          <li><strong>- 1x Kuota Pembuatan Institusi</strong></li>
+                          <li><strong>- 12+ Akun Multi Role (2x Akun Admin, 10x Akun Pengajar, Unlimited Akun Siswa)</strong></li> */}
+                        </ul>
+                        <div className="mt-auto w-full flex justify-end items-end">
+                          <a
+                            href="https://bit.ly/contact-evaloexam"
+                            className="w-full btn is-outline-black btn-animation is-large inline-block rounded justify-end items-end"
+                          >
+                            <span>Kontak Kami</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Feature Item */}
+                                    {/* Feature Item */}
+                                    <div className="jos" data-jos_delay={0}>
+                    <div className="hover-solid-shadow h-full">
+                      <div className="justify-start items-start h-full flex flex-col rounded-[10px] border-2 border-ColorBlack bg-white p-[30px]">
+                        <img
+                          src="https://masco-tailwind.netlify.app/assets/img/icons/icon-duotone-user-list.svg"
+                          alt="icon-duotone-user-list"
+                          width={60}
+                          height={60}
+                          className="mb-[30px] h-[60px] w-auto"
+                        />
+                        <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-2xl">
+                          Paket Institusi Kecil
+                        </div>
+                        <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-xl flex gap-2 items-center">
+                          <span className="line-through">IDR 2.743.110,-</span>  <Chip color="primary">IDR 1.371.555,- (50%)</Chip>
+                        </div>
+                        <p className="my-0"><strong>Yang akan anda dapatkan:</strong></p>
+                        <ul className="mb-5">
+                          <li><strong>- 20x Sesi Ujian</strong></li>
+                          <li><strong>- 15x Siswa per Sesi Ujian</strong></li>
+                          <li><strong>- 10x Soal per Sesi Siswa</strong></li>
+                          <li><strong>- 3.000 Soal di Evaluasi Otomatis </strong></li>
+                          {/* <li className="mt-4"><strong>Kuota Sistem:</strong></li>
+                          <li><strong>- 1x Kuota Pembuatan Institusi</strong></li>
+                          <li><strong>- 12+ Akun Multi Role (2x Akun Admin, 10x Akun Pengajar, Unlimited Akun Siswa)</strong></li> */}
                         </ul>
                         <div className="mt-auto w-full flex justify-end items-end">
                           <a
@@ -91,16 +169,16 @@ export default function Pricing() {
                           Paket Licensed Solution
                         </div>
                         <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-xl flex gap-2 items-center">
-                           <span className="line-through">IDR 64.999.999,-</span>  <Chip color="primary">IDR 42.249.999,- (35%)</Chip>
+                           <span className="line-through">IDR 65.919.000,-</span>  <Chip color="primary">IDR 32.959.500,- (50%)</Chip>
                         </div>
                         <p className="my-0"><strong>Yang akan anda dapatkan:</strong></p>
                         <ul className="mb-5">
                           <li><strong>- Lisensi Digital dan Salinan Produk untuk dijalankan di server sendiri</strong></li>
                           <li><strong>- Kostumisasi lebih lanjut pada produk yang lebih disesuaikan</strong></li>
                           <li><strong>- Maintanance Update Patch dan Security</strong></li>
-                          <li className="mt-4"><strong>Kuota Sistem:</strong></li>
+                          {/* <li className="mt-4"><strong>Kuota Sistem:</strong></li>
                           <li><strong>- Unlimited Kuota Pembuatan Institusi</strong></li>
-                          <li><strong>- Unlimited Akun Multi Role</strong></li>
+                          <li><strong>- Unlimited Akun Multi Role</strong></li> */}
                         </ul>
                         <div className="mt-auto w-full flex justify-end items-end">
                           <a
@@ -126,20 +204,20 @@ export default function Pricing() {
                           className="mb-[30px] h-[60px] w-auto"
                         />
                         <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-2xl">
-                          Paket Starter
+                          Paket per Exam
                         </div>
                         <div className="mb-4 text-xl font-semibold leading-[1.33] -tracking-[0.5px] text-black lg:text-xl flex gap-2 items-center">
-                          <span className="line-through">IDR 1.547.435,- / bulan </span>  <Chip color="primary">IDR 990.358,- (36%)</Chip>
+                          <span className="line-through">IDR 138.896,- / bulan </span>  <Chip color="primary">IDR 90.282,- (35%)</Chip>
                         </div>
                         <p className="my-0"><strong>Yang akan anda dapatkan (per bulan):</strong></p>
                         <ul className="mb-5">
-                          <li><strong>- 1x Upload Pelajaran Berbeda</strong></li>
-                          <li><strong>- 5x Ruang Ujian</strong></li>
-                          <li><strong>- 15x Sesi Siswa Mengerjakan</strong></li>
-                          <li><strong>- 225x Generasi dan Analisa soal menggunakan AI</strong></li>
-                          <li className="mt-4"><strong>Kuota Sistem:</strong></li>
+                          <li><strong>- 1x Sesi Ujian</strong></li>
+                          <li><strong>- 15x Siswa per Sesi Ujian</strong></li>
+                          <li><strong>- 10x Soal per Sesi Siswa</strong></li>
+                          <li><strong>- 150 Soal di Evaluasi Otomatis </strong></li>
+                          {/* <li className="mt-4"><strong>Kuota Sistem:</strong></li>
                           <li><strong>- 1x Kuota Pembuatan Institusi</strong></li>
-                          <li><strong>- 3+ Akun Multi Role (1x Akun Admin, 2x Akun Pengajar, Unlimited Akun Siswa)</strong></li>
+                          <li><strong>- 3+ Akun Multi Role (1x Akun Admin, 2x Akun Pengajar, Unlimited Akun Siswa)</strong></li> */}
                         </ul>
 
                         <div className="w-full mt-auto flex justify-end items-end">
